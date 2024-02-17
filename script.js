b6bef6f94810406f67c4910d8a5e8c27fa8da152
brcsilveira <contato.brunosilveira@gmail.com>
2024-02-17 12:56:19 -0300

    consertando os links dos contatos

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtém todos os links dentro das tags <li>
-    var links = document.querySelectorAll('li a');
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtém todos os links dentro das tags <li>
+  var links = document.querySelectorAll(".header li a");
 
-    // Itera sobre cada link
-    links.forEach(function(link) {
-        // Adiciona um ouvinte de evento de clique a cada link
-        link.addEventListener('click', function(e) {
-            // Previne o comportamento padrão do link
-            e.preventDefault();
+  // Itera sobre cada link
+  links.forEach(function (link) {
+    // Adiciona um ouvinte de evento de clique a cada link
+    link.addEventListener("click", function (e) {
+      // Previne o comportamento padrão do link
+      e.preventDefault();
 
-            // Obtém o alvo da âncora (o valor do atributo href)
-            var targetId = this.getAttribute('href').substring(1);
+      // Obtém o alvo da âncora (o valor do atributo href)
+      var targetId = this.getAttribute("href").substring(1);
 
-            // Obtém a seção correspondente
-            var targetSection = document.getElementById(targetId);
+      // Obtém a seção correspondente
+      var targetSection = document.getElementById(targetId);
 
-            // Rola suavemente para a seção
-            targetSection.scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
+      // Rola suavemente para a seção
+      targetSection.scrollIntoView({
+        behavior: "smooth",
+      });
     });
+  });
 });
